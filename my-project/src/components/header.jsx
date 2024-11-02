64636211e90051087e0c711577e60b54b040c39e
Haripriya-65 <kavyanshi9999@gmail.com>
2024-11-02 10:42:09 +0000

    added header

---
+++ my-project/src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function HeaderSection(){
+return(
+<div>
+<ul className="flex justify-evenly  font-semibold   ">
+<li>Home</li>
+<li>
+    About 
+</li>
+<li>Properties</li>
+<li> What is kumbh</li>
+<li>Gallery</li>
+<li>Contact</li>
+<li>Hi @admin</li>
+<li><button>Become a Host</button></li>
+</ul>
+
+</div>
+
+)
+
+}
